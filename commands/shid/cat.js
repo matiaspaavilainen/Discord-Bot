fdf33ad3d098771cf1ffd4bffd3c5a0d21e4c7c5
Matias Paavilainen <matias.paavilainen@gmail.com>
2023-08-23 15:13:53 +0300

    Rewrite in javascript

---
+++ commands/shid/cat.js
@@ -0,0 +1,61 @@
+const { SlashCommandBuilder } = require("discord.js");
+const snoowrap = require("snoowrap");
+require('dotenv').config()
+
+const reddit = new snoowrap({
+    user_agent: process.env.USER_AGENT,
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN
+})
+
+const subreddits = [
+    "OneOrangeBraincell",
+    "Catswithjobs",
+    "Catloaf",
+    "WhatsWrongWithYourCat",
+    "teefies",
+    "CatsOnKeyboards",
+    "blurrypicturesofcats",
+]
+
+let data = [];
+
+async function getSubmissions(n) {
+    for (const subreddit of subreddits) {
+        const hotPosts = await reddit.getHot(subreddit, {limit: n})
+        
+        hotPosts.forEach((post) => {
+            if (post.url.startsWith('https://i')) {
+                data.push({
+                    link: post.url,
+                });
+            };
+        });
+    };
+    console.log(`Found ${data.length} submission.`)
+    return;
+}
+
+async function getRandom() {
+    if (data.length === 0){
+        await getSubmissions(20);
+    }
+    const post = data[Math.floor(Math.random() * data.length)];
+    const index = data.indexOf(post);
+    if (index > -1) {
+        data.splice(index, 1)
+    };
+    return post.link;
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('cat')
+        .setDescription('Replies with a random cat image from reddit.'),
+    async execute(interaction) {
+        await interaction.deferReply();
+        await interaction.editReply(await getRandom());
+        
+    }
+};
